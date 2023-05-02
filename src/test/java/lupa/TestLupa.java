2a6442323cab0749f57d06bc749e25b65bf637b0
Gabriel conrado <strackersix@gmail.com>
2023-05-02 02:07:54 -0300

    Remoção dos comentários nos testes.

--- src/test/java/lupa/TestLupa.java
+++ src/test/java/lupa/TestLupa.java
@@ -22,37 +22,37 @@ public class TestLupa extends BaseTest implements Constantes {
         Assert.assertEquals(textInputPesquisar,pl.getTextInputPesquisar());
     }
 
-//    @Test
-//    @Story("Valdiar o placeholder do campo: 'Input'.")
-//    @Description("Validar a descrição 'Pesquisar...' do Placeholder com base na especificação técnica / regra de negócio / protótipo wireframe.")
-//    public void validarPlaceholderInputText(){
-//
-//        pl.clickBtnLupa();
-//
-//        Assert.assertEquals(textInputPesquisar,pl.getTextInputPesquisar());
-//    }
-//
-//    @Test
-//    @Story("Validar uma pesquisa com conteúdo 'Existente'.")
-//    @Description("Validar uma pesquisa buscando por um registro existente no blog e clicando no botão pesquisar.")
-//    public void validarPesquisaExistente(){
-//
-//        pl.clickBtnLupa();
-//        pl.senKeysTextoFinancas();
-//        pl.clickBtnPesquisar();
-//
-//        Assert.assertTrue(pl.getTextResultadoEncontrado().contains(pesquisaExistente));
-//    }
-//
-//    @Test
-//    @Story("Validar uma pesquisa com conteúdo 'Inexistente'.")
-//    @Description("Validar uma pesquisa buscando por um resgitro inexistente no blog, clicando no botão Pesquisar e validando a seguinte notificação retornada pelo blog: Nenhum resultado.")
-//    public void validarPesquisaInexistente(){
-//
-//        pl.clickBtnLupa();
-//        pl.entrarTextoAleatorioInputText();
-//        pl.clickBtnPesquisar();
-//
-//        Assert.assertEquals(nenhumResultado,pl.getTextNenhumResultado());
-//    }
+    @Test
+    @Story("Valdiar o placeholder do campo: 'Input'.")
+    @Description("Validar a descrição 'Pesquisar...' do Placeholder com base na especificação técnica / regra de negócio / protótipo wireframe.")
+    public void validarPlaceholderInputText(){
+
+        pl.clickBtnLupa();
+
+        Assert.assertEquals(textInputPesquisar,pl.getTextInputPesquisar());
+    }
+
+    @Test
+    @Story("Validar uma pesquisa com conteúdo 'Existente'.")
+    @Description("Validar uma pesquisa buscando por um registro existente no blog e clicando no botão pesquisar.")
+    public void validarPesquisaExistente(){
+
+        pl.clickBtnLupa();
+        pl.senKeysTextoFinancas();
+        pl.clickBtnPesquisar();
+
+        Assert.assertTrue(pl.getTextResultadoEncontrado().contains(pesquisaExistente));
+    }
+
+    @Test
+    @Story("Validar uma pesquisa com conteúdo 'Inexistente'.")
+    @Description("Validar uma pesquisa buscando por um resgitro inexistente no blog, clicando no botão Pesquisar e validando a seguinte notificação retornada pelo blog: Nenhum resultado.")
+    public void validarPesquisaInexistente(){
+
+        pl.clickBtnLupa();
+        pl.entrarTextoAleatorioInputText();
+        pl.clickBtnPesquisar();
+
+        Assert.assertEquals(nenhumResultado,pl.getTextNenhumResultado());
+    }
 }
